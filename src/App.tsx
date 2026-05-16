d6b426aa535cc6a734b8800c8ee8641a5cca2045
Carlos D Walker <60205826+Mrwealthyexp@users.noreply.github.com>
2026-05-16 07:23:59 -0400

    Refactor App component to remove Router
    Removed Router component wrapping the Layout and Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Domains from './pages/Domains'
@@ -6,14 +6,12 @@ import Register from './pages/Register'
 
 export default function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/domains" element={<Domains />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/domains" element={<Domains />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Layout>
   )
 }
